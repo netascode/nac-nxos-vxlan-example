27e61551ba8e5eb0c3757b7f75564c6c5078a445
danischm <danischm@cisco.com>
2026-05-01 18:01:16 +0200

    0.2.0 updates

--- main.tf
+++ main.tf
@@ -1,17 +1,7 @@
-terraform {
-  required_providers {
-    nxos = {
-      source = "CiscoDevNet/nxos"
-    }
-  }
-}
-
-# export NXOS_USERNAME=username
-# export NXOS_PASSWORD=password
-
 module "nxos" {
   source  = "netascode/nac-nxos/nxos"
-  version = "0.1.0"
+  version = "0.2.0"
 
   yaml_directories = ["data/"]
+  template_directories = ["templates/"]
 }
